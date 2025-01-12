31fd0775357ad9d78f83aa1ad2c4127195640d3c
omaymaalhwaarat <omaymaalhawarat@gmail.com>
2025-01-12 10:27:14 +0300

    youtubr API

---
+++ js/index.js
@@ -0,0 +1,12 @@
+const apiKey = 'AIzaSyBSeumWG2wRbg0YKkT64x43axyHGvJBSqk'; // ضع مفتاح API هنا
+const query = 'calculus';
+const apiUrl = `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${query}&type=video&key=${apiKey}`;
+
+fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+        const videoId = data.items[0].id.videoId; // الحصول على أول فيديو في النتائج
+        const iframe = document.getElementById('calculusVideo');
+        iframe.src = `https://www.youtube.com/embed/${videoId}`;
+    })
+    .catch(error => console.error('Error fetching YouTube API:', error));
